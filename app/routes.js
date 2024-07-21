9b789538e0d87019daba6ecd1900c71e06b3cf1e
Eddie Shannon <55087558+Ed209uk@users.noreply.github.com>
2024-07-21 23:27:54 +0100

    Multiple new pages and changes

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,10 @@
 const express = require('express');
 
 const router = express.Router();
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
 
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
