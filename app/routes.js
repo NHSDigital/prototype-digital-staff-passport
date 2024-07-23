43c851edaa889b01a4026c1f7682f677f879ed89
Eddie Shannon <55087558+Ed209uk@users.noreply.github.com>
2024-07-23 16:07:11 +0100

    Fixed routes.js

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,8 @@
 const express = require('express');
 
 const router = express.Router();
-const radioButtonRedirect = require('radio-button-redirect')
-router.use(radioButtonRedirect)
+const radioButtonRedirect = require('radio-button-redirect');
+router.use(radioButtonRedirect);
 
 // Add your routes here - above the module.exports line
 
