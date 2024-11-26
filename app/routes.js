e9795896fe1cab298708212132fb00c083f34867
Sophie Elliott <soel2@hscic.gov.uk>
2024-11-26 16:37:17 +0000

    Update pages but banner broken
    banner broken on index page

--- app/routes.js
+++ app/routes.js
@@ -6,5 +6,8 @@ const radioButtonRedirect = require('radio-button-redirect');
 router.use(radioButtonRedirect);
 
 // Add your routes here - above the module.exports line
+// Check whether the variable matches a condition
+
+    
 
 module.exports = router;
